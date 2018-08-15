8ad84494df6143480e332e755370d5c777c6cd64
Jim Watkins <jim@bigskyway.com>
2018-08-15 09:57:24 -0700

    Pretty close to done

---
+++ bweb/setup.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+apt-get update && apt-get install -y \
+        openssh-server \
+        nano \
+        vsftpd
+
+sed -i '/PasswordAuthentication yes/s/^#//g' /etc/ssh/sshd_config
+
+useradd -ms /bin/bash bridger 
+yes bridger | passwd bridger
+useradd -ms /bin/bash jim
+yes amanda | passwd jim
+
+systemctl enable ssh
